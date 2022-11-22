2dac1558b25c23ae56495270bd6de5ae9ef0fff0
TEXTBr <FireWitch2003@outlook.com>
2022-11-22 08:40:15 -0300

    tabela blocos com imagem d eusuario e botao de expandir adicionados

--- js/tabela.js
+++ js/tabela.js
@@ -8,9 +8,9 @@ botao_tabela.addEventListener('click', tabela_lista);
 function tabela_lista(){
   const tabela_blocos = document.getElementById("tabela-blocos");
   const tabela_lista = document.getElementById("tabela-lista");
-  const tabela_toda = document.getElementById("imagem-more-tabela")
+  const tabela_img_more = document.getElementById("imagem-more-tabela")
 
-  tabela_toda.style.display = 'block';
+  tabela_img_more.style.display = 'flex';
   tabela_lista.style.display = 'table';
   tabela_blocos.style.display = 'none';
 }
@@ -18,9 +18,9 @@ function tabela_lista(){
 function tabela_blocos(){
   const tabela_lista = document.getElementById('tabela-lista');
   const tabela_blocos = document.getElementById("tabela-blocos");
-  const tabela_toda = document.getElementById("imagem-more-tabela")
+  const tabela_img_more = document.getElementById("imagem-more-tabela")
 
-  tabela_toda.style.display = "none";
+  tabela_img_more.style.display = "none";
   tabela_lista.style.display = 'none';
   tabela_blocos.style.display = 'block';
 }
