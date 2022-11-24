8d4690e2e2a3f339bbf6b5a5c9d22974bcf0531f
Erick Ojeda <57963965+ErickOjeda@users.noreply.github.com>
2022-11-24 19:26:36 -0300

    Ajustes na home

--- js/home.js
+++ js/home.js
@@ -1,30 +1,25 @@
-const diasemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+// const diasemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+// const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
-const d = new Date();
+// const d = new Date();
 
-var element = document.getElementsByClassName('date')[0];
-element.innerHTML = diasemana[d.getDay()] + ', ' + d.getDate() + ' de ' + meses[d.getMonth()];
+// var element = document.getElementsByClassName('date')[0];
+// element.innerHTML = diasemana[d.getDay()] + ', ' + d.getDate() + ' de ' + meses[d.getMonth()];
 
 const ctx = document.getElementById('ocupacao');
+console.log(ctx);
 
 new Chart(ctx, {
-type: 'bar',
-data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-    label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    borderWidth: 1
-    }]
-},
-options: {
-    scales: {
-    y: {
-        beginAtZero: true
+    type: 'doughnut',
+    data: {
+        datasets: [{
+        data: [25, 75],
+        borderWidth: 1
+        }]
+    },
+    options: {
+       
     }
-    }
-}
 });
 
 
