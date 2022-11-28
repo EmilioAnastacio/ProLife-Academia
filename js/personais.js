6603fb7fed68304968febfe1c5000dae771043e5
Erick Ojeda <57963965+ErickOjeda@users.noreply.github.com>
2022-11-28 16:03:21 -0300

    Grid de personais

--- js/personais.js
+++ js/personais.js
@@ -9,13 +9,11 @@ botao_tabela.addEventListener('click', tabela_lista);
 function tabela_lista() {
   const tabela_blocos = document.getElementById("tabela-blocos");
   const tabela_lista = document.getElementById("tabela-lista");
-  const tabela_img_more = document.getElementById("imagem-more-tabela");
 
   tabela_img_blocos_altera.style.color = "black";
   tabela_img_lista_altera.style.color = "white";
-  botao_blocos.style.backgroundColor = "#D9D9D9";
-  botao_tabela.style.backgroundColor = "#004777";
-  tabela_img_more.style.display = 'flex';
+  botao_blocos.classList.remove("active");
+  botao_tabela.classList.add("active");
   tabela_lista.style.display = 'table';
   tabela_blocos.style.display = 'none';
 }
@@ -23,14 +21,11 @@ function tabela_lista() {
 function tabela_blocos() {
   const tabela_lista = document.getElementById('tabela-lista');
   const tabela_blocos = document.getElementById("tabela-blocos");
-  const tabela_img_more = document.getElementById("imagem-more-tabela");
-
 
   tabela_img_lista_altera.style.color = "black";
   tabela_img_blocos_altera.style.color = "white";
-  botao_blocos.style.backgroundColor = "#004777";
-  botao_tabela.style.backgroundColor = "#D9D9D9";
-  tabela_img_more.style.display = "none";
+  botao_blocos.classList.add("active");
+  botao_tabela.classList.remove("active");
   tabela_lista.style.display = 'none';
   tabela_blocos.style.display = 'block';
 }
@@ -43,46 +38,49 @@ botao_add.addEventListener("click", add_itens_tabela)
 
 
 function add_itens_tabela() {
-  contador_personais ++;
+  contador_personais++;
   const tabela = document.getElementById("tabela-lista")
   const corpo_tabela = document.getElementById("body-table");
 
   const blocos_tudo = document.getElementById("tabela-blocos");
-  const blocos_conteudos = document.getElementById("blocos-conteudo");  
+  const blocos_conteudos = document.getElementById("blocos-conteudo");
   const div_blocos = document.createElement("div");
-  div_blocos.className = "bloco_1";
+  div_blocos.className = "bloco";
 
   const tr = document.createElement("tr");
   tr.id = `${contador_personais}`
 
-  let campos = [
-    {
-      nome: window.prompt("digite o nome"),
-      cargo: window.prompt("digite o cargo"),
-      salario: window.prompt("digite o salario"),
-      url_img: "../img/botao_editar.png",
-      url_img_blocos:"../img/icone.png",
-      url_img_editar_blocos:"../img/Group_18.png"
-    }
-  ]
+  let campos =
+  {
+    nome: window.prompt("digite o nome").toUpperCase(),
+    cargo: window.prompt("digite o cargo"),
+    salario: window.prompt("digite o salario")
+  }
 
-  let conteudo = `<td>${campos[0].nome}</td>
-                  <td>${campos[0].cargo}</td>
-                  <td>$${campos[0].salario}</td>
-                  <td><img class="imagem-editar-abela" src="${campos[0].url_img}"alt=""></td>`
+  let conteudo = `<td>${contador_personais}</td>
+                  <td>${campos.nome}</td>
+                  <td>${campos.cargo}</td>
+                  <td>R$ ${campos.salario}</td>
+                  <td><i class="fa-solid fa-pen"></i></td>`
 
   tr.innerHTML = conteudo
   corpo_tabela.appendChild(tr);
   tabela.appendChild(corpo_tabela);
 
-  let conteudo_blocos = `<div>
-            <p>${campos[0].nome}</p> 
-            <p>${campos[0].cargo}</p>
-          </div>
-            <div class="imagem-icone-blocos">
-              <img src="${campos[0].url_img_blocos}" alt="">
-            </div>
-          <div class="imagem-editar-usuario"><img src="${campos[0].url_img_editar_blocos}" alt=""></div>`
+  let conteudo_blocos = 
+  `<div class="img-bloco">
+    <img src="../img/icone.png" alt="">
+  </div>
+
+  <div class="info-person">
+    <p>${campos.nome}</p>
+    <br>
+    <p>${campos.cargo}</p>
+  </div>
+
+  <div class="icon-open">
+    <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+  </div>`
 
   div_blocos.innerHTML = conteudo_blocos;
   blocos_conteudos.appendChild(div_blocos);
